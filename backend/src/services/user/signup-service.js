0d1b3014808b0adae96e9ca25e3c03b05136c1e2
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 14:49:57 -0300

    feat: create signUpService

---
+++ backend/src/services/user/signup-service.js
@@ -0,0 +1,50 @@
+import { saveUser } from "../../repository/user-mysql-repository.js";
+import { hashPassword } from "../../utils/bcrypt.js";
+import { generateToken } from "../../utils/jwt.js";
+
+async function signUpService(input) {
+  const { name, email, password } = input;
+  userValidations(input);
+  const hashedPassword = await hashPassword(password);
+  const userData = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  await saveUser(userData);
+
+  const generatedToken = generateToken({
+    name: userData.name,
+    email: userData.email,
+  });
+
+  return generatedToken;
+}
+
+function userValidations(input) {
+  const { name, email, password, passwordConfirmation } = input;
+
+  const properties = Object.entries(input);
+
+  for (const [key, value] of properties) {
+    if (typeof value !== "string") {
+      throw new Error(`Property ${key} value needs to be a string`);
+    }
+  }
+
+  const emailRgx = /\S+@\S+\.\S+/;
+  if (name.length < 3) {
+    throw new Error("Name is invalid.");
+  }
+
+  if (!emailRgx.test(email)) {
+    throw new Error("Email is invalid");
+  }
+
+  if (password !== passwordConfirmation) {
+    throw new Error("Password and password confirmation are different");
+  }
+}
+
+export default signUpService;
