268ecd704768db55e9543ff097bb055f1e565186
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 18:57:40 -0300

    fix: axios api

--- frontend/src/@global/api/api.ts
+++ frontend/src/@global/api/api.ts
@@ -4,9 +4,7 @@ export const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: apiUrl,
-  timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
