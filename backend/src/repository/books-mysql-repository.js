1171a0fd79ad289c9ba3c907abab57fa46fbb3a5
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 18:56:45 -0300

    fix: book table name in mysql repository

--- backend/src/repository/books-mysql-repository.js
+++ backend/src/repository/books-mysql-repository.js
@@ -6,7 +6,7 @@ export async function saveBook(input) {
   const { author, title, publishedAt, fileUrl } = input;
   const params = [author, title, publishedAt, fileUrl];
   const query =
-    "INSERT INTO TeacherAi.books (author, title, publishedAt, fileUrl) VALUES (?, ?, ?, ?)";
+    "INSERT INTO TeacherAi.book (author, title, publishedAt, fileUrl) VALUES (?, ?, ?, ?)";
   const [{ insertId }] = await pool.execute(query, params);
 
   return insertId;
