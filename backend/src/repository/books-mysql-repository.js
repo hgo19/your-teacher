34576c23941671afe8191f546c630aa10d71c459
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 16:45:24 -0300

    feat: create books mysql repository with saveBook method

---
+++ backend/src/repository/books-mysql-repository.js
@@ -0,0 +1,13 @@
+import connection from "../database/mysql/connection.js";
+
+const pool = connection;
+
+export async function saveBook(input) {
+  const { author, title, publishedAt, fileUrl } = input;
+  const params = [author, title, publishedAt, fileUrl];
+  const query =
+    "INSERT INTO TeacherAi.books (author, title, publishedAt, fileUrl) VALUES (?, ?, ?, ?)";
+  const [{ insertId }] = await pool.execute(query, params);
+
+  return insertId;
+}
