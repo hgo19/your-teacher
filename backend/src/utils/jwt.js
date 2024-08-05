cadc31edbd1530006f0433160d611903a1869a18
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 14:48:35 -0300

    feat: create jwt function to generate token

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
+
+const secret = process.env.SECRET_KEY;
+
+export async function generateToken(payload) {
+  const generatedToken = jwt.sign(payload, secret, {
+    expiresIn: "2h",
+    algorithm: "HS256",
+  });
+  return generatedToken;
+}
