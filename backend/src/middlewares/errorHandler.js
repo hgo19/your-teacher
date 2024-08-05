8c8a8baa953ca80626e98e92f5159a310cf5dd90
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 19:05:49 -0300

    feat: updated errorHandler middleware

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,4 +1,11 @@
+import AbstractError from "../errors/AbstractErrors";
+
 export const ErrorHandler = (error, req, res) => {
   console.log(error);
-  return res.status(500).json({ message: error.message });
+  if (error instanceof AbstractError) {
+    const { statusCode, message } = error;
+    return res.status(statusCode).json({ message });
+  }
+
+  return res.status(500).json({ message: "Internal server error" });
 };
