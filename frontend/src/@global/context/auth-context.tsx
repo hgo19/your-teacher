a8d65688f73f4bbc28b638df6f456b70c675a35f
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 20:52:23 -0300

    fix: auth-context

--- frontend/src/@global/context/auth-context.tsx
+++ frontend/src/@global/context/auth-context.tsx
@@ -11,7 +11,9 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [token, setToken] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token"),
+  );
 
   useEffect(() => {
     const savedToken = localStorage.getItem("token");
