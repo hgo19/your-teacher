5eef1cef8574179f752cae951db20cae795e3fc1
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 17:31:38 -0300

    feat: add cors on backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoute from "./routes/user.routes.js";
 import booksRoute from "./routes/books.routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", userRoute);
 app.use("/books", booksRoute);
