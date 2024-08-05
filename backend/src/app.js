a879a51d7803f04b3692bdd47e2bd1753b5a0902
Hugo Leonardo <huleonardo19@gmail.com>
2024-08-05 18:44:59 -0300

    fix: errorHandler

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRoute from "./routes/user.routes.js";
 import booksRoute from "./routes/books.routes.js";
 import cors from "cors";
-import { errorHandler } from "./middlewares/errorHandler.js";
+import { ErrorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +11,6 @@ app.use(cors());
 app.use("/", userRoute);
 app.use("/books", booksRoute);
 
-app.use(errorHandler);
+app.use(ErrorHandler);
 
 export default app;
--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-export const errorHandler = (req, res, next, error) => {
+export const ErrorHandler = (error, req, res) => {
   console.log(error);
   return res.status(500).json({ message: error.message });
 };
